5c89a0cac8bd7017d93f6135586c393375bcab16
Abdullahh98 <114352755+Abdullahh98@users.noreply.github.com>
2025-04-16 13:40:57 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,23 +2,19 @@ import numpy as np
 import pandas as pd
 import regex as re
 import joblib
-import spacy
-
+import spacy  # ✅ use spacy instead of en_core_web_sm
 
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.svm import LinearSVC
 
-nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("en_core_web_sm")  # ✅ this will load the model from installed package
 classifier = LinearSVC()
 
 def clean_text(text):
-    # reduce multiple spaces and newlines to only one
     text = re.sub(r'(\s\s+|\n\n+)', r'\1', text)
-    # remove double quotes
     text = re.sub(r'"', '', text)
-
     return text
-	
+
 def convert_text(text):
     sent = nlp(text)
     ents = {x.text: x for x in sent.ents}
@@ -30,18 +26,9 @@ def convert_text(text):
             tokens.append(w.text)
         else:
             tokens.append(w.lemma_.lower())
-    text = ' '.join(tokens)
-
-    return text
-
+    return ' '.join(tokens)
 
 class preprocessor(TransformerMixin, BaseEstimator):
-
-    def __init__(self):
-        pass
-
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X):
-        return X.apply(clean_text).apply(convert_text)
+    def __init__(self): pass
+    def fit(self, X, y=None): return self
+    def transform(self, X): return X.apply(clean_text).apply(convert_text)
