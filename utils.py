cb67884664e1f3ba5a11049ca1e1e5f0d72a14da
Abdullahh98 <114352755+Abdullahh98@users.noreply.github.com>
2025-04-16 13:31:18 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,17 +1,23 @@
 import numpy as np
 import pandas as pd
 import regex as re
-import spacy
-from sklearn.base import BaseEstimator, TransformerMixin
+import joblib
+import en_core_web_sm
 
-# Load spaCy model globally
-nlp = spacy.load("en_core_web_sm")
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.svm import LinearSVC
+
+nlp = en_core_web_sm.load()
+classifier = LinearSVC()
 
 def clean_text(text):
+    # reduce multiple spaces and newlines to only one
     text = re.sub(r'(\s\s+|\n\n+)', r'\1', text)
+    # remove double quotes
     text = re.sub(r'"', '', text)
-    return text
 
+    return text
+	
 def convert_text(text):
     sent = nlp(text)
     ents = {x.text: x for x in sent.ents}
@@ -23,9 +29,16 @@ def convert_text(text):
             tokens.append(w.text)
         else:
             tokens.append(w.lemma_.lower())
-    return ' '.join(tokens)
+    text = ' '.join(tokens)
+
+    return text
+
 
 class preprocessor(TransformerMixin, BaseEstimator):
+
+    def __init__(self):
+        pass
+
     def fit(self, X, y=None):
         return self
 
