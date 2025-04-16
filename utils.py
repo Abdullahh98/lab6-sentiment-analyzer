9744578b9efaf4c5b107d6264f5bda16467a3e06
Abdullahh98 <114352755+Abdullahh98@users.noreply.github.com>
2025-04-16 13:37:47 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,20 +2,22 @@ import numpy as np
 import pandas as pd
 import regex as re
 import joblib
-import spacy  # ✅ use spacy instead of en_core_web_sm
+import en_core_web_sm
 
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.svm import LinearSVC
 
-nlp = spacy.load("en_core_web_sm")  # ✅ use string name
-
+nlp = en_core_web_sm.load()
 classifier = LinearSVC()
 
 def clean_text(text):
+    # reduce multiple spaces and newlines to only one
     text = re.sub(r'(\s\s+|\n\n+)', r'\1', text)
+    # remove double quotes
     text = re.sub(r'"', '', text)
-    return text
 
+    return text
+	
 def convert_text(text):
     sent = nlp(text)
     ents = {x.text: x for x in sent.ents}
@@ -27,9 +29,18 @@ def convert_text(text):
             tokens.append(w.text)
         else:
             tokens.append(w.lemma_.lower())
-    return ' '.join(tokens)
+    text = ' '.join(tokens)
+
+    return text
+
 
 class preprocessor(TransformerMixin, BaseEstimator):
-    def __init__(self): pass
-    def fit(self, X, y=None): return self
-    def transform(self, X): return X.apply(clean_text).apply(convert_text)
+
+    def __init__(self):
+        pass
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        return X.apply(clean_text).apply(convert_text)
